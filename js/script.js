ea2585837cd6adfcd257f40079c8c5edfd9d812d
sdb07khan <ksaddab@gmail.com>
2023-04-18 18:34:59 +0530

    details page added, responsive left

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Time Picker HIGHLIGHT
+
+$("#timepicker").timepicker({
+  timeFormat: "h:mm p",
+  interval: 60,
+  minTime: "7",
+  maxTime: "10:00pm",
+  defaultTime: "Time to Call",
+  startTime: "07:00",
+  dynamic: false,
+  dropdown: true,
+  scrollbar: true,
+});
+
+// Slick Slider HIGHLIGHT
+$(".similarPostBox-slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  buttons: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
